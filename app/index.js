537ab12d0ad72657e3cdfa18549cf9a7d52acc19
Reidar Sollid <rsollid@gmail.com>
2020-01-09 20:06:21 +0100

    Added wacken day counter

--- app/index.js
+++ app/index.js
@@ -2,11 +2,12 @@ import document from "document";
 import clock from "clock";
 
 let myClock = document.getElementById("myClock");
-
+let wackenCounter = document.getElementById("wacken-counter");
 clock.granularity = 'seconds'; // seconds, minutes, hours
 
-clock.ontick = function(evt) {
+clock.ontick = function (evt) {
     myClock.text = ("0" + evt.date.getHours()).slice(-2) + ":" +
         ("0" + evt.date.getMinutes()).slice(-2) + ":" +
         ("0" + evt.date.getSeconds()).slice(-2);
+    wackenCounter.text = ("00" + evt.date.getDay()).slice(-3);
 };
