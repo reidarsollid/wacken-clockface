de50368ac7458c064c162daa6176ca7e69bc5186
Reidar Sollid <rsollid@gmail.com>
2020-01-10 09:52:32 +0100

    Calculate days

--- app/index.js
+++ app/index.js
@@ -3,11 +3,21 @@ import clock from "clock";
 
 let myClock = document.getElementById("myClock");
 let wackenCounter = document.getElementById("wacken-counter");
+let end = new Date("2020-07-30");
 clock.granularity = 'seconds'; // seconds, minutes, hours
 
 clock.ontick = function (evt) {
+    //Difference in miliseconds
+    let today = evt.date;
+    //Difference in miliseconds
+    let diff = Math.abs(end - today);
+    let years = parseInt(diff / 31536000000);
+    diff -= years * 31536000000;
+
+    let days = parseInt(diff / 86400000);
+
     myClock.text = ("0" + evt.date.getHours()).slice(-2) + ":" +
         ("0" + evt.date.getMinutes()).slice(-2) + ":" +
         ("0" + evt.date.getSeconds()).slice(-2);
-    wackenCounter.text = ("00" + evt.date.getDay()).slice(-3);
+    wackenCounter.text = ("0" + days).slice(-3);
 };
