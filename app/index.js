fc691401b1dd5db8f1bee99cab7387300313951c
Reidar Sollid <rsollid@gmail.com>
2020-01-13 23:29:44 +0100

    Added bullhead background

--- app/index.js
+++ app/index.js
@@ -2,6 +2,7 @@ import document from "document";
 import clock from "clock";
 
 let myClock = document.getElementById("myClock");
+let myClock2 = document.getElementById("myClock2");
 let wackenCounter = document.getElementById("wacken-counter");
 let end = new Date("2020-07-30");
 clock.granularity = 'seconds'; // seconds, minutes, hours
@@ -16,8 +17,10 @@ clock.ontick = function (evt) {
 
     let days = parseInt(diff / 86400000);
 
-    myClock.text = ("0" + evt.date.getHours()).slice(-2) + ":" +
+    let clockText= ("0" + evt.date.getHours()).slice(-2) + ":" +
         ("0" + evt.date.getMinutes()).slice(-2) + ":" +
         ("0" + evt.date.getSeconds()).slice(-2);
+    myClock.text = clockText;
+    myClock2.text = clockText;
     wackenCounter.text = ("0" + days).slice(-3);
 };
